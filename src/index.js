1eadb84c2cf35c544662c485867ac64a63ac215a
JK <jk.floods@gmail.com>
2023-02-14 10:52:54 +0100

    Export FormSelect

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import FormSelect from './lib/FormSelect'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FormSelect />
-  </React.StrictMode>
-)
+export { FormSelect };
