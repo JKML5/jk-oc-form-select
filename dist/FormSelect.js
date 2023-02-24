de471ede976d49a8e73315d27ab04767eede8296
JK <jk.floods@gmail.com>
2023-02-24 10:02:05 +0100

    npm run build

--- dist/FormSelect.js
+++ dist/FormSelect.js
@@ -10,18 +10,20 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 var _templateObject;
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+var StyledSelect = _styledComponents.default.select(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  font-size: 16px;\n  height: 40px;\n  border: none;\n  outline: 1px solid #000000;\n  border-radius: 5px;\n  background-color: #ffffff;\n  padding: 0 10px;\n  width: 50%;\n\n  &:focus {\n    border: none;\n    outline: 2px solid #4e6701;\n  }\n"])));
 function FormSelect(_ref) {
   var id = _ref.id,
     name = _ref.name,
+    value = _ref.value,
+    onChange = _ref.onChange,
     data = _ref.data;
-  var StyledSelect = _styledComponents.default.select(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n    font-size: 16px;\n    height: 40px;\n    border: none;\n    outline: 1px solid #000000;\n    border-radius: 5px;\n    background-color: #ffffff;\n    padding: 0 10px;\n    width: 50%;\n\n    &:focus {\n      border: none;\n      outline: 2px solid #4e6701;\n    }\n  "])));
-  var sortedValues = typeof data[0] === 'string' ? data.sort() : data.sort(function (a, b) {
+  var sortedValues = typeof data[0] === "string" ? data.sort() : data.sort(function (a, b) {
     if (Object.values(a)[0] < Object.values(b)[0]) return -1;
     if (Object.values(a)[0] > Object.values(b)[0]) return 1;
     return 0;
   });
   function getOption(optionData) {
-    return typeof optionData === 'string' ? /*#__PURE__*/_react.default.createElement("option", {
+    return typeof optionData === "string" ? /*#__PURE__*/_react.default.createElement("option", {
       key: optionData,
       value: optionData
     }, optionData) : /*#__PURE__*/_react.default.createElement("option", {
@@ -31,19 +33,27 @@ function FormSelect(_ref) {
   }
   return /*#__PURE__*/_react.default.createElement(StyledSelect, {
     id: id,
-    name: name
-  }, sortedValues.map(function (value) {
-    return getOption(value);
+    name: name,
+    value: value,
+    onChange: onChange
+  }, /*#__PURE__*/_react.default.createElement("option", {
+    value: ""
+  }, "\xA0"), sortedValues.map(function (optionData) {
+    return getOption(optionData);
   }));
 }
 FormSelect.defaultProps = {
-  id: '',
-  name: '',
+  id: "",
+  name: "",
+  value: "",
+  onChange: function onChange() {},
   data: []
 };
 FormSelect.propTypes = {
   id: _propTypes.default.string,
   name: _propTypes.default.string,
+  value: _propTypes.default.string,
+  onChange: _propTypes.default.func,
   data: _propTypes.default.oneOfType([
   // Accepte à la fois un tableau de string et un tableau d'objets
   _propTypes.default.arrayOf(_propTypes.default.string), _propTypes.default.arrayOf(_propTypes.default.shape({}))])
